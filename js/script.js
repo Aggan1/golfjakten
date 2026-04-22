87c93672b3ee1b64a1f2a5c8bb0225ef534a2e7a
Axel Nilsson <an225ab@student.lnu.se>
2026-04-22 18:02:37 +0200

    Visade golfbanor från SMAPI på sidan istället för konsol

--- js/script.js
+++ js/script.js
@@ -1,17 +1,65 @@
-const url = "https://smapi.lnu.se/api/?api_key=EB72AuVs&controller=activity&method=getall&descriptions=golfbana";
+const url =
+
+    "https://smapi.lnu.se/api/?api_key=EB72AuVs&controller=activity&method=getall&descriptions=golfbana";
+
+const golfList = document.getElementById("golf-list");
 
 fetch(url)
 
-    .then(response => response.json())
+    .then(function (response) {
 
-    .then(data => {
-
-        console.log("SMAPI data:", data);
+        return response.json();
 
     })
 
-    .catch(error => {
+    .then(function (data) {
+
+        console.log("SMAPI data:", data);
+
+        if (data.header.status === "OK") {
+
+            renderGolfCourses(data.payload);
+
+        } else {
+
+            golfList.innerHTML = "<p>Kunde inte hämta golfbanor.</p>";
+
+        }
+
+    })
+
+    .catch(function (error) {
 
         console.log("Fel vid hämtning:", error);
 
+        golfList.innerHTML = "<p>Något gick fel vid hämtning av data.</p>";
+
     });
+
+function renderGolfCourses(courses) {
+
+    golfList.innerHTML = "";
+
+    courses.forEach(function (course) {
+
+        const card = document.createElement("article");
+
+        card.classList.add("golf-card");
+
+        card.innerHTML = `
+
+      <h3>${course.name}</h3>
+
+      <p>${course.description}</p>
+
+      <p>Betyg: ${course.rating}</p>
+
+      <p>Antal recensioner: ${course.num_reviews}</p>
+
+    `;
+
+        golfList.appendChild(card);
+
+    });
+
+}
