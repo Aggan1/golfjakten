2ac25027204873de752d2f9a85647c04f98deb8e
Ängla Fast <af224kk@student.lnu.se>
2026-05-13 12:55:15 +0200

    La in en karta och gjorde om css på golfresesidan

--- js/golfresor.js
+++ js/golfresor.js
@@ -1,6 +1,12 @@
-  const price = document.getElementById("price");
-  const priceValue = document.getElementById("price-value");
+const map = L.map("map").setView([62.0, 15.0], 5);
 
-  price.addEventListener("input", () => {
-    priceValue.textContent = price.value + " kr";
-  });
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap"
+}).addTo(map);
+
+const price = document.getElementById("price");
+const priceValue = document.getElementById("price-value");
+
+price.addEventListener("input", function () {
+  priceValue.textContent = price.value + " kr";
+});
