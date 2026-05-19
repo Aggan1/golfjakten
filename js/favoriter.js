485d1767614ba4406d17526872d37296d8fab421
Axel Nilsson <an225ab@student.lnu.se>
2026-05-19 21:37:39 +0200

    Gjorde så att sparade golfbanor visas på favorit sidan samt att man kan ta bort dem och kunna klicka in på läsmer knapp för nästa funtkion

--- js/favoriter.js
+++ js/favoriter.js
@@ -1,1 +1,58 @@
-console.log("Favoritsidan kopplad rätt test :)");
+import { getData } from "./api.js";
+import { getExtraData, mergaGolfdata } from "./golfData.js";
+import { getFavorites } from "./favoriterStorage.js";
+import { renderFavoriter } from "./renderFavoriter.js";
+
+const favoritList = document.getElementById("favorit-list");
+const antalFavoriter = document.getElementById("antal-favoriter");
+const sortering = document.getElementById("sortering");
+
+let allCourses = [];
+
+function getSavedCourses() {
+    const favoriteIds = getFavorites();
+
+    return allCourses.filter(function (bana) {
+        return favoriteIds.includes(bana.id);
+    });
+}
+
+function sortCourses(courses) {
+    if (sortering.value === "az") {
+        courses.sort(function (a, b) {
+            return a.name.localeCompare(b.name, "sv");
+        });
+    }
+
+    if (sortering.value === "za") {
+        courses.sort(function (a, b) {
+            return b.name.localeCompare(a.name, "sv");
+        });
+    }
+
+    return courses;
+}
+
+function updatePage() {
+    let savedCourses = getSavedCourses();
+    savedCourses = sortCourses(savedCourses);
+
+    antalFavoriter.textContent = savedCourses.length;
+
+    renderFavoriter(savedCourses, favoritList, updatePage);
+}
+
+function init() {
+    Promise.all([
+        getData("establishment", { descriptions: "golfbana" }),
+        getExtraData()
+    ])
+        .then(function (resultat) {
+            allCourses = mergaGolfdata(resultat[0], resultat[1]);
+            updatePage();
+        });
+
+    sortering.addEventListener("change", updatePage);
+}
+
+init();
